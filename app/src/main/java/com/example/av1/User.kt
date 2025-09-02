2136adf4a4d6a468e47cdc7a34368fb2ca8eac4e
GuilhermeA <guimenegari27@gmail.com>
2025-09-02 18:56:51 -0300

    Added the body of the project

---
+++ app/src/main/java/com/example/av1/User.kt
@@ -0,0 +1,10 @@
+package com.example.av1
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val userName: String,
+    val points: Int
+): Parcelable
