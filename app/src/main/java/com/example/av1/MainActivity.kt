6ad8183922243f5898dea9c91790a151492226b8
GuilhermeA <guimenegari27@gmail.com>
2025-09-02 19:28:17 -0300

    feat(Actvty1) Login Implemented

--- app/src/main/java/com/example/av1/MainActivity.kt
+++ app/src/main/java/com/example/av1/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.av1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editNome: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        editNome = this.findViewById(R.id.editNome)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun goNextActivity(view: View) {
+        val userInput = editNome.text.toString()
+
+        if (userInput.isBlank()) {
+            editNome.error = "Por favor, insira seu nome"
+            return
+        }
+
+        val intent = Intent(this, Activity2::class.javaClass)
+
+        val user = User (
+            userName = userInput,
+            points = 0
+        )
+
+        intent.putExtra("user", user)
+
+        startActivity(intent)
     }
 }
